6df8d7f0e8d7ec723092b8733afb4d7525aa28ce
clemensenit <155433572+clemensenit@users.noreply.github.com>
2026-02-01 15:02:44 +0100

    make it available in local network

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react()],
   base: '/cycle-tracker/',
+  server: {
+    host: true,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
